5f2e16e2c4aded0254db43d35552de7fd644b65b
gabrielavmattos <gmattos@thoughtworks.com>
2018-03-25 18:17:30 -0300

    Start to deselopment FoodTracker example

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Gabriela Mattos on 25/03/18.
+//  Copyright © 2018 Gabriela Mattos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
