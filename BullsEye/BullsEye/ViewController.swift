21d646d7ef2711f1c5002ae63f2ec8fbfd312026
gabrielavmattos <gmattos@thoughtworks.com>
2018-03-26 17:54:23 -0300

    Include icon and name app

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
 
@@ -79,6 +80,13 @@ class ViewController: UIViewController {
         rounds = 0
         totalScore = 0
         startNewRound()
+        
+        let transition = CATransition()
+        transition.type = kCATransitionFade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        
+        view.layer.add(transition, forKey: nil)
     }
     
     func getNumberRandom() -> Int {
