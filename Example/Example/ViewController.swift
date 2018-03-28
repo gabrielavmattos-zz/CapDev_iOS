c667ddf4ae4ae5e7b733ccca39793451b7240145
gabrielavmattos <gmattos@thoughtworks.com>
2018-03-28 10:07:40 -0300

    Create new project to example using TableView

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Gabriela Mattos on 28/03/18.
+//  Copyright © 2018 thoughtworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        return cell
+    }
+
+
+}
+
