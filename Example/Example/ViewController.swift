c65597c92dfd7c0e490b130ac4c3270e127d03d0
gabrielavmattos <gmattos@thoughtworks.com>
2018-03-28 10:31:52 -0300

    Example using TableView that implements UITableViewDataSource and UITableViewDelegate

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -8,30 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    let data = ["Item1", "Item2", "Item3", "Item4", "Item5"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+}
+// ViewController+UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
         
+        let item = data[indexPath.row]
+        
+        cell.textLabel?.text = item
+        
         return cell
     }
-
-
+    
 }
 
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
